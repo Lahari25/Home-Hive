55251eeb785f047a1af60324a7f57987f6a62ff3
SamiAhmed007 <84227987+SamiAhmed007@users.noreply.github.com>
2024-04-29 22:33:25 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+    // Shared script for interacting with database
+    // hpalaparthi1
+    $servername = "localhost";
+    $username = "hpalaparthi1"; // change to panther id if using codd
+    $password = "hpalaparthi1"; // change to panther id if using codd
+    $dbname = "hpalaparthi1"; // change to panther id if using codd
+    // The above used four variables servername,username,password and dbname 
+    // are according to my environment setting.
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("connection failed: " . $conn->connect_error);
+    }
+?>
---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title>Home Hive</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="css/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="css/nivo-lightbox.css">
+  <link rel="stylesheet" href="css/nivo_themes/default/default.css">
+
+  <link rel="stylesheet" href="css/hover-min.css">
+  <link rel="stylesheet" href="css/flexslider.css">
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+  <div class="preloader">
+    <div class="sk-spinner sk-spinner-pulse"></div>
+  </div>
+
+  <section id="home" class="parallax-section">
+    <div class="gradient-overlay"></div>
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-offset-2 col-md-8 col-sm-12">
+          <a href="index_choose.php" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll"
+            data-wow-delay="1.3s">REGISTER</a>
+          <h1 class="wow fadeInUp" data-wow-delay="0.6s">Home Hive</h1>
+          <p class="wow fadeInUp" data-wow-delay="1.0s" style="background-color: white;">"Transform Your Property Dreams into Reality: Discover, Connect, and Nest at Home Hive!"</p>
+          <a href="buyer_login.php" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll"
+            data-wow-delay="1.3s">BUY PROPERTY</a>
+          <a href="seller_login.php" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll"
+            data-wow-delay="1.3s">LIST PROPERTY</a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- Feature section -->
+  <section id="feature" class="parallax-section">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10">
+          <div class="wow fadeInUp section-title" data-wow-delay="0.6s">
+            <h2>Why Choose Us?</h2>
+          </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div class="feature-thumb">
+            <div class="feature-icon">
+            </div>
+            <h3>SEAMLESS TRANSFERS</h3>
+            <p>
+            Home Hive streamlines the property journey, offering a frictionless and relaxed experience for all users. Effortless transactions are now just a tap away!
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+          <div class="feature-thumb">
+            <div class="feature-icon">
+            </div>
+            <h3>EXTENSIVE CONNECTIONS</h3>
+            <p>
+            Dive into a Vibrant Community: Connect with a Diverse Network and Discover Your Ideal Property Match at Home Hive!
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.9s">
+          <div class="feature-thumb">
+            <div class="feature-icon">
+            </div>
+            <h3>TRUSTED LISTINGS</h3>
+            <p>
+            Confidence in Every Connection: Home Hive Guarantees Verified Listings for a Secure and Trustworthy Property Journey!
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <!-- About section -->
+  <section id="about" class="parallax-section">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10">
+          <div class="wow fadeInUp section-title" data-wow-delay="0.3s">
+            <h2>Spotlight Showcase: Home Hive's Popular Listings, Handpicked for Your Exploration!</h2>
+            <h4>Schedule Your Home Hive Mansions Tour: Connect with Us for a Personalized Site Visit Experience!</h4>
+          </div>
+        </div>
+ 
+        <div class="clearfix"></div>
+
+        <div class="wow fadeInUp col-md-3 col-sm-5" data-wow-delay="0.3s">
+          <img src="images/about-img.jpg" class="img-responsive" alt="About">
+        </div>
+
+        <div class="wow fadeInUp col-md-5 col-sm-7" data-wow-delay="0.5s">
+
+          <!-- flexslider -->
+          <div class="flexslider">
+            <ul class="slides">
+
+              <li>
+                <img src="images/slide-img1.jpg" alt="Flexslider">
+              </li>
+              <li>
+                <img src="images/slide-img2.jpg" alt="Flexslider">
+              </li>
+              <li>
+                <img src="images/slide-img3.jpg" alt="Flexslider">
+              </li>
+
+            </ul>
+          </div>
+        </div>
+
+        <div class="wow fadeInUp col-md-4 col-sm-12" data-wow-delay="0.9s">
+
+          <ul>
+          
+            <li>One Million Empowered Property Enthusiasts</li>
+            <li>Backed by Trustworthy Sellers</li>
+            <li>Authentic Property Imagery</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer section -->
+  <footer>
+    <div class="container">
+      <div class="row">
+
+        <div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="1.3s">
+          <h3>Office Address</h3>
+          <p>2450 Lindbergh PL NE, 30324</p>
+        </div>
+
+        <div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="1.6s">
+          <h3>Contact Details</h3>
+          <p>201-423-7899</p>
+          <p>homehive@company.com</p>
+        </div>
+
+        <div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="1.9s">
+          <h3>Opening Hours</h3>
+          <strong>Monday - Firday</strong>
+          <p>10:00 AM - 10:00 PM</p>
+          <strong>Saturday - Sunday</strong>
+          <p>9:00 AM - 09:00 PM</p>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+  <!-- Copyright section -->
+  <section id="copyright">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-8 col-sm-8 col-xs-8">
+          <p>Copyright © Lahari and Sami
+        </div>
+
+        <div class="col-md-4 col-sm-4 text-right">
+          <a href="#top" class="fa fa-angle-up smoothScroll gototop"></a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!-- javscript js -->
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <script src="js/jquery.magnific-popup.min.js"></script>
+
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.backstretch.min.js"></script>
+
+  <script src="js/isotope.js"></script>
+  <script src="js/imagesloaded.min.js"></script>
+  <script src="js/nivo-lightbox.min.js"></script>
+
+  <script src="js/jquery.flexslider-min.js"></script>
+
+  <script src="js/jquery.parallax.js"></script>
+  <script src="js/smoothscroll.js"></script>
+  <script src="js/wow.min.js"></script>
+
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
